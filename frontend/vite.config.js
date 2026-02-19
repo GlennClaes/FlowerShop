07ed739de34a5b34ed32c2495685a3479bf5970f
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2026-02-19 14:55:26 +0100

    Compose fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,6 +17,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['src/__tests__/**/*.spec.js'], 
+    include: ['src/__tests__/**/*.spec.js'],
   }
 })
