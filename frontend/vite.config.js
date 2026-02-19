ceaa384284c90d3b69491e7e7e2eb2f5e87113a3
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2026-02-19 14:41:35 +0100

    Bugfix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,6 +17,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['src/__tests__/**/*.spec.js'], // <-- voeg dit toe
+    include: ['src/__tests__/**/*.spec.js'], 
   }
 })
