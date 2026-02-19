f39a851562158b9a83d6e0b7d006c5f02bd3e273
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2026-02-19 16:56:17 +0100

    Fix: base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/FlowerShop/',
+  base: '/',
   plugins: [
     vue(),
     vueJsx()
